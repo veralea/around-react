dfb59474cd3fc7f4ff01c09cd45313117ee8046a
veralea <veralea@rambler.ru>
2021-11-16 00:05:28 +0200

    feat: all features of Card

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import '../App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { api } from "../utils/api";
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
  
 
 function App() {
@@ -12,6 +14,23 @@ function App() {
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
   const [selectedCard, setSelectedCard] = useState(null);
+  const [currentUser, setCurrentUser] = useState(
+    {
+      name: 'Vera Lea', 
+      about: 'Teacher', 
+      avatar: 'https://cdn.pixabay.com/photo/2018/01/17/07/06/laptop-3087585_960_720.jpg', 
+      _id: 'c77e00360c598eb801029d0f', 
+      cohort: 'group-12'
+    }
+  );
+
+  useEffect(() => {
+    api.getUserInfo()    
+    .then((result) => {
+      setCurrentUser(result); 
+    })
+    .catch((err) => console.log(err));
+  },[]);
 
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(true);
@@ -39,63 +58,65 @@ function App() {
   return (
     <div className="page">
       <div className="page__content">
-        <Header />
-        <Main 
-          onEditProfileClick={handleEditProfileClick} 
-          onAddPlaceClick={handleAddPlaceClick}
-          onEditAvatarClick={handleEditAvatarClick}
-          onCardClick={handleCardClick}
-        />
-        <PopupWithForm 
-          name='update-avatar' 
-          title='Change profile picture' 
-          isOpen={isEditAvatarPopupOpen}
-          onClose={closeAllPopups}
-          buttonText='Save'
-        >  
-          <input type="url" className="popup__input" name="avatar" defaultValue="" placeholder="Avatar link" required />
-          <span className="popup__error"></span>    
-        </PopupWithForm>
-        <PopupWithForm 
-          name='edit' 
-          title='Edit profile' 
-          isOpen={isEditProfilePopupOpen}
-          onClose={closeAllPopups}
-          buttonText='Save'
-        >
-          <input type="text" className="popup__input"
-            name="name" defaultValue="" minLength="2" maxLength="40" required />
-          <span className="popup__error"></span>
-          <input type="text" className="popup__input"
-            name="job" defaultValue="" minLength="2" maxLength="200" required />
-          <span className="popup__error"></span>
-        </PopupWithForm>
-        <PopupWithForm 
-          name='add' 
-          title='New place' 
-          isOpen={isAddPlacePopupOpen}
-          onClose={closeAllPopups}
-          buttonText='Create'
-        >
-          <input type="text" className="popup__input" name="name" defaultValue=""
-            placeholder="Title" minLength="1" maxLength="30" required />
-          <span className="popup__error"></span>
-          <input type="url" className="popup__input" name="link" defaultValue="" placeholder="Image link" required />
-          <span className="popup__error"></span>
-        </PopupWithForm>
-        <PopupWithForm 
-            name='delete' 
-            title='Are you sure?' 
-            isOpen={false}
+        <CurrentUserContext.Provider value={currentUser}>
+          <Header />
+          <Main 
+            onEditProfileClick={handleEditProfileClick} 
+            onAddPlaceClick={handleAddPlaceClick}
+            onEditAvatarClick={handleEditAvatarClick}
+            onCardClick={handleCardClick}
+          />
+          <PopupWithForm 
+            name='update-avatar' 
+            title='Change profile picture' 
+            isOpen={isEditAvatarPopupOpen}
             onClose={closeAllPopups}
-            buttonText='Yes'
-        />
-        <ImagePopup 
-          card={selectedCard}
-          isOpen={selectedCard ? true : false}
-          onClose={closeAllPopups}
-        />          
-        <Footer />
+            buttonText='Save'
+          >  
+            <input type="url" className="popup__input" name="avatar" defaultValue="" placeholder="Avatar link" required />
+            <span className="popup__error"></span>    
+          </PopupWithForm>
+          <PopupWithForm 
+            name='edit' 
+            title='Edit profile' 
+            isOpen={isEditProfilePopupOpen}
+            onClose={closeAllPopups}
+            buttonText='Save'
+          >
+            <input type="text" className="popup__input"
+              name="name" defaultValue="" minLength="2" maxLength="40" required />
+            <span className="popup__error"></span>
+            <input type="text" className="popup__input"
+              name="job" defaultValue="" minLength="2" maxLength="200" required />
+            <span className="popup__error"></span>
+          </PopupWithForm>
+          <PopupWithForm 
+            name='add' 
+            title='New place' 
+            isOpen={isAddPlacePopupOpen}
+            onClose={closeAllPopups}
+            buttonText='Create'
+          >
+            <input type="text" className="popup__input" name="name" defaultValue=""
+              placeholder="Title" minLength="1" maxLength="30" required />
+            <span className="popup__error"></span>
+            <input type="url" className="popup__input" name="link" defaultValue="" placeholder="Image link" required />
+            <span className="popup__error"></span>
+          </PopupWithForm>
+          <PopupWithForm 
+              name='delete' 
+              title='Are you sure?' 
+              isOpen={false}
+              onClose={closeAllPopups}
+              buttonText='Yes'
+          />
+          <ImagePopup 
+            card={selectedCard}
+            isOpen={selectedCard ? true : false}
+            onClose={closeAllPopups}
+          />          
+          <Footer />
+        </CurrentUserContext.Provider>
       </div>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { api } from "../utils/api";
 import Card from './Card'; 
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Main(props) {
-  const [userName, setUserName] = useState();
-  const [userDescription, setUserDescription] = useState();
-  const [userAvatar , setUserAvatar] = useState();
-  const [cards , setCards] = useState([]);  
+  const [cards , setCards] = useState([]);
+  const currentUser = useContext(CurrentUserContext); 
 
   useEffect(() => {
-    api.getUserInfo()    
-    .then((result) => {
-        setUserName(result.name);
-        setUserDescription(result.about);
-        setUserAvatar(result.avatar);
-    })
-    .catch((err) => console.log(err));
-    
     api.getCards()    
     .then((result) => {
         setCards(result);
@@ -24,24 +15,42 @@ function Main(props) {
     .catch((err) => console.log(err))
   },[]);
 
+  function handleCardLike(card) {
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    
+    api.changeLikes(card._id, !isLiked ? "PUT" : "DELETE").then((newCard) => {
+        setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+    });
+  }
+
+  function handleCardDelete(card) {
+
+    api.deleteCard(card._id).then(() => {
+      const newCards = cards.filter((c) => c._id !== card._id);
+      setCards(newCards);
+    });
+    
+    
+  }
+
   return(
     <main className="main">
         <section className="profile">
             <div 
                 className="profile__avatar" 
                 onClick={props.onEditAvatarClick}
-                style={{ backgroundImage: `url(${userAvatar})` }}
+                style={{ backgroundImage: `url(${currentUser.avatar})` }}
             >
               <div className="profile__avatar-cover"></div>    
             </div>
             <div className="profile__title">
-              <span className="profile__name">{userName}</span>
+              <span className="profile__name">{currentUser.name}</span>
               <button className="button profile__edit-button" aria-label="edit button"
                   onClick={props.onEditProfileClick}>
               <div className="profile__edit-image"></div>
               </button>
             </div>
-            <p className="profile__job">{userDescription}</p>
+            <p className="profile__job">{currentUser.about}</p>
             <button className="button profile__add-button" aria-label="add button" 
               onClick={props.onAddPlaceClick}>
               <div className="profile__add-image"></div>
@@ -55,7 +64,10 @@ function Main(props) {
                   <Card 
                     key={card._id} 
                     card={card} 
-                    onCardClick={props.onCardClick}/>
+                    onCardClick={props.onCardClick}
+                    onCardLike={(e) => handleCardLike(card)}
+                    onCardDelete={(e) => handleCardDelete(card)}
+                  />
                 );
               })
             }
